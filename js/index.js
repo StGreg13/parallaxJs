570448d4828f072bca2d1b135470222aec4fb306
StGreg13 <13mrgreg@gmail.com>
2020-10-06 17:57:55 +0300

    maket update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-$('document').ready(function() {
-	var options = { videoId: 'NKCwkOIKMkc', start: 3 };
-	$('.wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-});
+// $('document').ready(function() {
+// 	var options = { videoId: 'RL9WprlbkXo', start: 3 };
+// 	$('.wrapper').tubular(options);
+// 	// f-UGhWj1xww cool sepia hd
+// 	// 49SKbS7Xwf4 beautiful barn sepia
+// });
